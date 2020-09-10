bb80583616c63f95cd29edcd0b67a94b492a33ae
Bùi Trung Quân <buitrungquan812001@gmail.com>
2020-09-11 02:21:53 +0700

    Fix bug

--- modules/api/endpoints/add.py
+++ modules/api/endpoints/add.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter
 from modules.file.files.fileAttributes import FileAttributes
 from modules.file.dirs.dir import DIR_PATH
-from os import path
+import os
 
 route = APIRouter()
 
 @route.post("/")
 def AddFile(file: FileAttributes):
 	path = DIR_PATH[file.kind.value] + file.filename
-	if path.isfile(path):
+	if os.path.isfile(path):
 		return {"error": "File existed"}
 	with open(path, "w") as f:
 		size = f.write(file.content)
--- modules/api/endpoints/update.py
+++ modules/api/endpoints/update.py
@@ -2,14 +2,14 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from modules.file.files.fileAttributes import FileAttributes
 from modules.file.dirs.dir import DIR_PATH
-from os import path
+import os
 
 route = APIRouter()
 
 @route.put("/")
 def Update(file: FileAttributes):
 	path = DIR_PATH[file.kind.value] + file.filename
-	if not path.isfile(path):
+	if not os.path.isfile(path):
 		return JSONResponse(status_code = 404, content = {"error": "File not found"})
 	with open(path, "w") as f:
 		size = f.write(file.content)
